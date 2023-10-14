3e7ee7c3a89e3f1f1f63c713f12535313056285c
Leonardo Iglesias <leo-im@hotmail.com>
2023-10-13 21:05:58 -0500

    add images export

---
+++ src/img/carousel/index.ts
@@ -0,0 +1,5 @@
+import Carousel31 from "./1.jpeg?jsx";
+import Carousel32 from "./2.jpeg?jsx";
+import Carousel33 from "./3.jpeg?jsx";
+
+export { Carousel31, Carousel32, Carousel33 };
---
+++ src/img/section-3/index.ts
@@ -0,0 +1,5 @@
+import Section31 from "./1.jpeg?jsx";
+import Section32 from "./2.jpeg?jsx";
+import Section33 from "./3.jpeg?jsx";
+
+export { Section31, Section32, Section33 };
