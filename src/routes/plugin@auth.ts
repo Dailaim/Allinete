d774530387978d421f56e88723c65f5c78cd3f45
Leonardo Iglesias <leo-im@hotmail.com>
2023-10-15 11:32:51 -0500

    add plugin auth

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,4 +1,5 @@
 import type { AuthConfig } from "@auth/core";
+import Email from "@auth/core/providers/email";
 
 import GitHub from "@auth/core/providers/github";
 import { serverAuth$ } from "@builder.io/qwik-auth";
@@ -12,10 +13,22 @@ export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
 			({
 				secret: env.get("AUTH_SECRET"),
 				trustHost: true,
+
 				providers: [
 					GitHub({
 						clientId: env.get("GITHUB_ID"),
 						clientSecret: env.get("GITHUB_SECRET"),
+					}),
+					Email({
+						server: {
+							host: env.get("EMAIL_SERVER_HOST"),
+							port: env.get("EMAIL_SERVER_PORT"),
+							auth: {
+								user: env.get("EMAIL_SERVER_USER"),
+								pass: env.get("EMAIL_SERVER_PASSWORD"),
+							},
+						},
+						from: env.get("EMAIL_FROM"),
 					}),
 				],
 				adapter: SurrealDBAdapter(db as unknown as Promise<SurrealWebSocket>),
