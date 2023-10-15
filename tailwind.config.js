f55ce8e902d22bcb9a2ca633ba44b72cd1af6d3a
Leonardo Iglesias <leo-im@hotmail.com>
2023-10-15 11:31:38 -0500

    update for custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,35 @@ module.exports = {
 				"press-start": ['"Press Start 2P"', "cursive"],
 				merriweather: ["Merriweather", "serif"],
 				mulish: ["Mulish", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			},
+
+			colors: {
+				"blue-gray": "#697586",
+				gray: "#dfe1e3",
+				"off-white": "#F9F9FC",
+				purple: { DEFAULT: "#b0a6bd", 400: "#dfdbe5", 200: "#efedf2" },
+				pink: {
+					DEFAULT: "#f5a3b7",
+					light: "#fee2e3",
+					300: "#fff7f7",
+				},
+				green: {
+					DEFAULT: "#7fccc6",
+					100: "#f3fafa",
+				},
+				red: {
+					DEFAULT: "#e77373",
+					100: "#fdf1f1",
+				},
+				orange: {
+					DEFAULT: "#fdb64e",
+					100: "#fff8ee",
+				},
 			},
 		},
 	},
-	plugins: [require("daisyui")],
+	plugins: [
+		/* require("daisyui") */
+	],
 };
