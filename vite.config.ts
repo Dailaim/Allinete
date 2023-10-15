e3a17a0654d640cb7233412cf4c40016c6f7201b
Leonardo Iglesias <leo-im@hotmail.com>
2023-10-15 11:31:51 -0500

    add svg to tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
+import svgx from "@svgx/vite-plugin-qwik";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
 	return {
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		plugins: [qwikCity(), qwikVite(), tsconfigPaths(), svgx()],
 		preview: {
 			headers: {
 				"Cache-Control": "public, max-age=600",
