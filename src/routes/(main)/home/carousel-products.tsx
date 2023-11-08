0e718396a037078de92cf7e408df368dfb908d9d
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-11-07 22:08:22 -0500

    Add virtual module to Swiper import

--- src/routes/(main)/home/carousel-products.tsx
+++ src/routes/(main)/home/carousel-products.tsx
@@ -3,10 +3,11 @@ import {
 	component$,
 	useId,
 	useStyles$,
+	useTask$,
 	useVisibleTask$,
 } from "@builder.io/qwik";
 import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination, Virtual } from "swiper/modules";
 import {
 	PaginationCss,
 	SwiperContainer,
@@ -19,95 +20,92 @@ export const CarouselProducts = component$(() => {
 	useStyles$(PaginationCss);
 	const id = useId();
 
-	useVisibleTask$(
-		() => {
-			new Swiper(`.${id}`, {
-				modules: [Pagination, Navigation],
-				autoplay: true,
-				loop: true,
-				autoHeight: true,
-				navigation: {
-					nextEl: `.swiper-button-next-${id}`,
-					prevEl: `.swiper-button-prev-${id}`,
+	useVisibleTask$(() => {
+		new Swiper(`.${id}`, {
+			modules: [Pagination, Navigation],
+			autoplay: true,
+			loop: true,
+			autoHeight: true,
+			navigation: {
+				nextEl: `.swiper-button-next-${id}`,
+				prevEl: `.swiper-button-prev-${id}`,
+			},
+
+			pagination: {
+				el: `.swiper-pagination-${id}`,
+				clickable: true,
+			},
+
+			breakpoints: {
+				300: {
+					slidesPerView: 1.2,
+					spaceBetween: 10,
+				},
+				390: {
+					slidesPerView: 1.4,
+					spaceBetween: 10,
+				},
+				450: {
+					slidesPerView: 1.6,
+					spaceBetween: 10,
+				},
+				550: {
+					slidesPerView: 1.8,
+					spaceBetween: 10,
+				},
+				640: {
+					slidesPerView: 2.5,
+					spaceBetween: 10,
 				},
 
-				pagination: {
-					el: `.swiper-pagination-${id}`,
-					clickable: true,
+				900: {
+					slidesPerView: 2.8,
+					spaceBetween: 10,
 				},
 
-				breakpoints: {
-					300: {
-						slidesPerView: 1.2,
-						spaceBetween: 10,
-					},
-					390: {
-						slidesPerView: 1.4,
-						spaceBetween: 10,
-					},
-					450: {
-						slidesPerView: 1.6,
-						spaceBetween: 10,
-					},
-					550: {
-						slidesPerView: 1.8,
-						spaceBetween: 10,
-					},
-					640: {
-						slidesPerView: 2.5,
-						spaceBetween: 10,
-					},
-
-					900: {
-						slidesPerView: 2.8,
-						spaceBetween: 10,
-					},
-
-					1024: {
-						slidesPerView: 3.2,
-						spaceBetween: 10,
-					},
-
-					1400: {
-						slidesPerView: 4,
-						spaceBetween: 10,
-					},
+				1024: {
+					slidesPerView: 3.2,
+					spaceBetween: 10,
 				},
-			});
-		},
-		{
-			strategy: "document-ready",
-		},
-	);
+
+				1400: {
+					slidesPerView: 4,
+					spaceBetween: 10,
+				},
+			},
+		});
+	});
 
 	return (
-		<SwiperContainer preFix={id} class="overflow-hidden relative">
-			<Slot />
+		<>
+			<SwiperContainer preFix={id} class="overflow-hidden relative">
+				<Slot />
 
-			<Decorators q:slot="decorators">
-				<div class="absolute top-0 -right-4 z-10 w-12 blur h-full bg-off-white" />
-				<SVGCircleArrowLeft
-					class={[
-						`swiper-button-prev-${id}`,
-						"absolute top-1/3 bottom-1/2 left-0 z-40 opacity-80 text-purple-400 hover:text-purple",
-					]}
-				/>
+				<Decorators q:slot="decorators">
+					<div class="absolute top-0 -right-4 z-10 w-12 blur h-full bg-off-white" />
+					<SVGCircleArrowLeft
+						class={[
+							`swiper-button-prev-${id}`,
+							"absolute top-1/3 bottom-1/2 left-0 z-40 opacity-80 text-purple-400 hover:text-purple",
+						]}
+					/>
 
-				<SVGCircleArrowRight
-					class={[
-						`swiper-button-next-${id}`,
-						"absolute top-1/3 bottom-1/2 right-0 z-40 opacity-90 text-purple-400 hover:text-purple",
-					]}
-				/>
+					<SVGCircleArrowRight
+						class={[
+							`swiper-button-next-${id}`,
+							"absolute top-1/3 bottom-1/2 right-0 z-40 opacity-90 text-purple-400 hover:text-purple",
+						]}
+					/>
 
-				<div
-					class={[
-						`swiper-pagination-${id}`,
-						"mt-5 w-full mx-auto flex items-center justify-center",
-					]}
-				/>
-			</Decorators>
-		</SwiperContainer>
+					<div
+						class={[
+							`swiper-pagination-${id}`,
+							"mt-5 w-full mx-auto flex items-center justify-center",
+						]}
+					/>
+				</Decorators>
+			</SwiperContainer>
+		</>
 	);
 });
 
