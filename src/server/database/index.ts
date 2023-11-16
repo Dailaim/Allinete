5cdfa6acc4a339f18d05046d0f5b0c24dc2c623a
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-11-15 22:30:41 -0500

    Add server-side database connection and middleware request handler.

--- src/server/database/index.ts
+++ src/server/database/index.ts
@@ -1,24 +1,52 @@
+import { server$ } from "@builder.io/qwik-city";
+import { EnvGetter } from "@builder.io/qwik-city/middleware/request-handler";
 import { Surreal } from "surrealdb.js";
 
-export const db = new Surreal();
+export const database = new Surreal();
 
-async function main() {
-	try {
-		// Use any of these 3 connect methods to connect to the database
-		// 1.Connect to the database
-		await db.connect("http://127.0.0.1:8080/rpc");
+export const connect = {
+	value: false,
+};
 
-		// Signin as a namespace, database, or root user
-		await db.signin({
-			user: "root",
-			pass: "root",
+export const DB = server$(async function (env?: EnvGetter) {
+	if (connect.value) {
+		return database;
+	}
+
+	const getEnv = env?.get ?? this?.env?.get;
+
+	if (!getEnv) {
+		throw new Error("No env getter found");
+	}
+
+	const url = getEnv("DB_URL") ?? "";
+	const user = getEnv("DB_USER") ?? "";
+	const pass = getEnv("DB_PASSWORD") ?? "";
+	const name = getEnv("DB_NAME") ?? "";
+	const namespace = getEnv("DB_NAMESPACE") ?? "";
+
+	// Use any of these 3 connect methods to connect to the database
+	// 1.Connect to the database
+	await database.connect(url).catch((err) => {
+		console.error(err, "Error connecting to database");
+	});
+
+	// Signin as a namespace, database, or root user
+	await database
+		.signin({
+			user: user,
+			pass: pass,
+		})
+		.catch((err) => {
+			console.error(err, "Error signing into database");
 		});
 
-		// Select a specific namespace / database
-		await db.use({ ns: "test", db: "test" });
-	} catch (e) {
-		console.error("ERROR Connection Database", e);
-	}
-}
+	// Select a specific namespace / database
+	await database.use({ ns: namespace, db: name }).catch((err) => {
+		console.error(err, "Error selecting namespace");
+	});
 
-main();
+	connect.value = true;
+
+	return database;
+});
