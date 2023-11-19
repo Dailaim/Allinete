ff01a3a0f36925432a0fcdcffefe22db6f24950d
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-11-19 18:30:07 -0500

    Add motion to mobile navbar

--- apps/front/src/components/navbar/navbar.tsx
+++ apps/front/src/components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 import type { QwikTouchEvent, Signal } from "@builder.io/qwik";
-import { $, component$, useSignal } from "@builder.io/qwik";
+import { $, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
+import { Motion } from "~/packages/motion";
 import { ButtonText } from "../button";
 import {
 	SVGAccountWithName,
@@ -68,25 +69,48 @@ export const NavbarMobile = component$(() => {
 
 	const CartSig = useSignal(false);
 
+  useVisibleTask$(({track})=>{
+    track(()=>MenuSig.value)
+    track(()=>CartSig.value)
+
+    if (MenuSig.value || CartSig.value) {
+      document.body.style.overflow = "hidden";
+      document.body.style.height = "100vh";
+    }
+
+    if (!MenuSig.value && !CartSig.value) {
+      document.body.style.overflow = "auto";
+      document.body.style.height = "auto";
+    }
+
+  })
+
 	const { handleTouchStart, handleTouchMove, handleTouchEnd } = useMenuTouch(
 		MenuSig,
 		CartSig,
 	);
 
 	return (
-		<>
+    <>
+    <span class="overflow-x-hidden ">
 			<MenuMobile MenuSig={MenuSig} />
+      <CartMobile MenuSig={CartSig} />
+    </span>
 			<div
 				onTouchStart$={handleTouchStart}
 				onTouchMove$={handleTouchMove}
 				onTouchEnd$={handleTouchEnd}
-				class="lg:hidden flex  px-5 py-2.5 fixed bottom-0 w-full bg-white shadow-md shadow-black sh select-none"
+				class="lg:hidden flex overflow-x-hidden max-w-[100vw] px-5 py-2.5 fixed bottom-0 w-full bg-white shadow-md shadow-black sh select-none"
 			>
 				<div class="container mx-auto items-center justify-around w-full flex">
 					<SVGMenuWithName
 						class=""
 						onClick$={() => {
-							MenuSig.value = !MenuSig.value;
+              if (CartSig.value) {
+                CartSig.value = false;
+              }
+              MenuSig.value = !MenuSig.value;
+              
 						}}
 					/>
 
@@ -94,12 +118,99 @@ export const NavbarMobile = component$(() => {
 						Alinette
 					</Link>
 					<span>
-						<SVGCartWithName class=" hover:text-pink" />
+						<SVGCartWithName
+              onClick$={() => {
+              if (MenuSig.value) {
+                MenuSig.value = false;
+              }
+                CartSig.value = !CartSig.value;
+              }
+              }
+            class=" hover:text-pink" />
 					</span>
 				</div>
 			</div>
+     
 		</>
 	);
+});
+
+export const CartMobile = component$<{
+	MenuSig: Signal<boolean>;
+}>(({ MenuSig }) => {
+	const { handleTouchStart, handleTouchMove, handleTouchEnd } =
+		useMenuTouch(undefined, MenuSig);
+
+
+	return (
+    <Motion.div
+      onTouchStart$={handleTouchStart}
+      onTouchMove$={handleTouchMove}
+      onTouchEnd$={handleTouchEnd}
+      class={{
+        "fixed -z-10 flex h-screen w-screen flex-col justify-start bg-white lg:hidden":
+          true,
+      }}
+      initial={{
+        transform: "translateX(100%)",
+
+      }}
+      animate={{
+        transform: MenuSig.value ? "translateX(0)" : "translateX(100%)",
+
+      }}
+    >
+      <div class="flex w-full select-none items-center justify-between bg-pink px-5 py-4 uppercase text-white">
+        <h2 class="text-2xl font-semibold">Cart</h2>
+
+        <div
+          class="cursor-pointer pl-10 "
+          onClick$={() => {
+            MenuSig.value = false;
+          }}
+        >
+          <svg
+            class="inline"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M14.9966 1.00613L1.0083 14.9915"
+              stroke="white"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M15 15L1 1"
+              stroke="white"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        onClick$={() => {
+          MenuSig.value = false;
+        }}
+        class="flex  w-full flex-col justify-start gap-5 px-5 py-6 uppercase"
+      >
+        <SVGSearchWithName noTextSize row class="gap-1 text-lg" />
+
+        <SVGAccountWithName noTextSize row class="gap-1 text-lg" />
+
+        <Link class="text-lg hover:text-pink">Shop all</Link>
+        <Link class="text-lg hover:text-pink">BESTSELLERS</Link>
+        <Link class="text-lg hover:text-pink">COLLECTION</Link>
+        <Link class="text-lg hover:text-pink">ABOUT US</Link>
+      </div>
+    </Motion.div>
+  );
 });
 
 export const MenuMobile = component$<{
@@ -109,15 +220,20 @@ export const MenuMobile = component$<{
 		useMenuTouch(MenuSig);
 
 	return (
-		<div
+		<Motion.div
 			onTouchStart$={handleTouchStart}
 			onTouchMove$={handleTouchMove}
 			onTouchEnd$={handleTouchEnd}
-			class="absolute w-screen h-screen bg-white flex flex-col justify-start lg:hidden"
-			style={{
-				transform: MenuSig.value ? "translateX(0)" : "translateX(-100%)",
-				transition: "transform 0.3s ease-in-out",
+			class="fixed w-screen h-screen bg-white flex flex-col justify-start lg:hidden"
+
+      initial={{
+        transform: "translateX(-100%)",
+      }}
+
+			animate={{
+				transform: MenuSig.value ? ["translateX(-100%)","translateX(0)"] : "translateX(-100%)",
 			}}
+
 		>
 			<div class="w-full bg-pink flex px-5 py-4 text-white uppercase justify-between items-center select-none">
 				<h2 class="font-semibold text-2xl">Menu</h2>
@@ -168,7 +284,7 @@ export const MenuMobile = component$<{
 				<Link class="hover:text-pink text-lg">COLLECTION</Link>
 				<Link class="hover:text-pink text-lg">ABOUT US</Link>
 			</div>
-		</div>
+		</Motion.div>
 	);
 });
 
