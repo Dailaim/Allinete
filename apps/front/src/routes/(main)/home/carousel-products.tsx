b6a82bdb32f6b1dbf690145eb2ba4603cb61cd43
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-04 17:50:50 -0500

    Refactor carousel products component

--- apps/front/src/routes/(main)/home/carousel-products.tsx
+++ apps/front/src/routes/(main)/home/carousel-products.tsx
@@ -1,121 +1,82 @@
-import {
-	Slot,
-	component$,
-	useId,
-	useStyles$,
-	useVisibleTask$,
-} from "@builder.io/qwik";
-import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
-import {
-	PaginationCss,
-	SwiperContainer,
-	SwiperCss,
-} from "~/components/carousel/swiper";
-import { SVGCircleArrowLeft, SVGCircleArrowRight } from "~/components/icons";
+import { Slot, component$ } from "@builder.io/qwik";
+
+import { SwiperContainer } from "~/components/carousel/swiper";
 
 export const CarouselProducts = component$(() => {
-	useStyles$(SwiperCss);
-	useStyles$(PaginationCss);
-	const id = useId();
+	const breakpoints = {
+		300: {
+			slidesPerView: 1.2,
+			spaceBetween: 10,
+		},
+		390: {
+			slidesPerView: 1.4,
+			spaceBetween: 10,
+		},
+		450: {
+			slidesPerView: 1.6,
+			spaceBetween: 10,
+		},
+		550: {
+			slidesPerView: 1.8,
+			spaceBetween: 10,
+		},
+		640: {
+			slidesPerView: 2.5,
+			spaceBetween: 10,
+		},
 
-	useVisibleTask$(() => {
-		new Swiper(`.${id}`, {
-			modules: [Pagination, Navigation],
-			autoplay: true,
-			loop: true,
+		900: {
+			slidesPerView: 2.8,
+			spaceBetween: 10,
+		},
 
-			autoHeight: true,
-			navigation: {
-				nextEl: `.swiper-button-next-${id}`,
-				prevEl: `.swiper-button-prev-${id}`,
-			},
-			centeredSlides: true,
-			pagination: {
-				el: `.swiper-pagination-${id}`,
-				clickable: true,
-			},
-			speed: 300,
-			breakpoints: {
-				300: {
-					slidesPerView: 1.2,
-					spaceBetween: 10,
-				},
-				390: {
-					slidesPerView: 1.4,
-					spaceBetween: 10,
-				},
-				450: {
-					slidesPerView: 1.6,
-					spaceBetween: 10,
-				},
-				550: {
-					slidesPerView: 1.8,
-					spaceBetween: 10,
-				},
-				640: {
-					slidesPerView: 2.5,
-					spaceBetween: 10,
-				},
+		1024: {
+			slidesPerView: 3.2,
+			spaceBetween: 10,
+		},
+		1250: {
+			slidesPerView: 3.7,
+			spaceBetween: 10,
+		},
 
-				900: {
-					slidesPerView: 2.8,
-					spaceBetween: 10,
-				},
-
-				1024: {
-					slidesPerView: 3.2,
-					spaceBetween: 10,
-				},
-				1250: {
-					slidesPerView: 3.7,
-					spaceBetween: 10,
-				},
-
-				1400: {
-					slidesPerView: 4,
-					spaceBetween: 10,
-				},
-				1500: {
-					slidesPerView: 4.6,
-					spaceBetween: 10,
-				},
-			},
-		});
-	});
+		1400: {
+			slidesPerView: 4,
+			spaceBetween: 10,
+		},
+		1500: {
+			slidesPerView: 4.6,
+			spaceBetween: 10,
+		},
+	};
 
 	return (
-		<span class="relative">
-			<div class="absolute top-0 -right-6 z-10 w-12 blur h-full bg-off-white" />
-			<div class="absolute top-0 -left-6 z-10 w-12 blur h-full bg-off-white" />
-			<SwiperContainer preFix={id} class="overflow-hidden relative">
+		<>
+			<SwiperContainer
+				modules={{
+					navigation: true,
+					pagination: true,
+				}}
+				breakpoints={breakpoints}
+				speed={300}
+				centeredSlides
+				autoHeight
+				loop
+				autoPlay
+				init
+				pagination
+				navigation
+				class="overflow-x-hidden relative"
+			>
 				<Slot />
-
-				<Decorators q:slot="decorators">
-					<SVGCircleArrowLeft
-						class={[
-							`swiper-button-prev-${id}`,
-							"absolute top-1/3 bottom-1/2 left-0 z-40 opacity-80 text-purple-400 hover:text-purple",
-						]}
-					/>
-
-					<SVGCircleArrowRight
-						class={[
-							`swiper-button-next-${id}`,
-							"absolute top-1/3 bottom-1/2 right-0 z-40 opacity-90 text-purple-400 hover:text-purple",
-						]}
-					/>
-
-					<div
-						class={[
-							`swiper-pagination-${id}`,
-							"mt-5 w-full mx-auto flex items-center justify-center",
-						]}
-					/>
-				</Decorators>
+				<div
+					q:slot="decorators"
+					class="absolute top-0 -right-6 z-10 w-12 blur h-full bg-off-white"
+				/>
+				<div
+					q:slot="decorators"
+					class="absolute top-0 -left-6 z-10 w-12 blur h-full bg-off-white"
+				/>
 			</SwiperContainer>
-		</span>
+		</>
 	);
 });
-
-const Decorators = component$(() => <Slot />);
