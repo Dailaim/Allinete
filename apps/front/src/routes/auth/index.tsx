475b98a4629eec5cf9a77b622603c43ed5f9ba39
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-06 08:19:33 -0500

    Refactor authentication component and add sign-in functionality

--- apps/front/src/routes/auth/index.tsx
+++ apps/front/src/routes/auth/index.tsx
@@ -1,19 +1,46 @@
-import { component$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
+import { component$, useSignal } from "@builder.io/qwik";
+import { Form, server$ } from "@builder.io/qwik-city";
 import { useAuthSignin } from "~/routes/plugin@auth";
+import wretch from "wretch"
+
 
 export default component$(() => {
 	const signIn = useAuthSignin();
 
+	const response  = useSignal("")
+
 	return (
-		<Form action={signIn}>
-			<input type="hidden" name="providerId" value="github" />
-			<input
-				type="hidden"
-				name="options.callbackUrl"
-				value="https://localhost:5173/store"
-			/>
-			<button type="submit">Sign In</button>
-		</Form>
+		<>
+		<div class="h-60">
+			{response.value}
+		</div>
+			<button type="button" onClick$={async ()=>{
+				response.value = await wretch().options({
+					credentials: 'include',
+				})
+				.url("http://localhost:3000/signin").post({
+				email: "leo",
+				password: "leo"
+				}).unauthorized((error)=>{
+					console.log(error)
+					return "unauthorized"
+				})
+
+				.text()
+
+				console.log(response.value)
+			}}>
+				Sign In
+			</button>
+			<Form action={signIn}>
+				<input type="hidden" name="providerId" value="github" />
+				<input
+					type="hidden"
+					name="options.callbackUrl"
+					value="https://localhost:5173/store"
+				/>
+				<button type="submit">Sign In</button>
+			</Form>
+		</>
 	);
 });
