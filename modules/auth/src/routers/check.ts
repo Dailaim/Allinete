9c76f90cf2e02d4be05b2d3b2e1892b409b98dff
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-10 13:25:17 -0500

    Refactor checkAuth router

--- modules/auth/src/routers/check.ts
+++ modules/auth/src/routers/check.ts
@@ -2,46 +2,26 @@ import { prismaAuth } from "@database/auth";
 import { Elysia, NotFoundError, t } from "elysia";
 import { AccessToken } from "../const";
 
-export const checkAuth = new Elysia().post(
-	"/check",
-	async ({ cookie, set }) => {
-		const token = cookie[AccessToken]?.get();
+export const checkAuth = new Elysia().onAfterHandle(async ({ cookie }) => {
+	const token = cookie[AccessToken]?.get();
 
-		if (!token) {
-			return new NotFoundError("Token not found");
-		}
+	if (!token) {
+		return new NotFoundError("Token not found");
+	}
 
-		const ctx = new prismaAuth();
-		const sess = await ctx.session.findUnique({
-			where: {
-				sessionToken: token,
-				AND: {
-					expires: {
-						gt: new Date(),
-					},
+	const ctx = new prismaAuth();
+	const sess = await ctx.session.findUnique({
+		where: {
+			sessionToken: token,
+			AND: {
+				expires: {
+					gt: new Date(),
 				},
 			},
-		});
-
-		if (!sess) {
-			return new NotFoundError("Session not found");
-		}
-
-		return {
-			message: "Authorized",
-		};
-	},
-	{
-		response: {
-			200: t.Object({
-				message: t.String(),
-			}),
-			401: t.Object({
-				error: t.String(),
-			}),
 		},
-		cookie: t.Cookie({
-			[AccessToken]: t.Optional(t.String()),
-		}),
-	},
-);
+	});
+
+	if (!sess) {
+		return new NotFoundError("Session not found");
+	}
+});
