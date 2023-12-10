828c634cc154ed9b18e2299a95bb1ab377c9e4e0
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-10 11:49:28 -0500

    Refactor auth module initialization

--- modules/auth/src/init.ts
+++ modules/auth/src/init.ts
@@ -1,7 +1,19 @@
+import { logger } from "@bogeychan/elysia-logger";
+import { handlerErrors } from "@utils/elysia";
 import { Elysia, t } from "elysia";
 import { authApp } from ".";
 
-const app = new Elysia().use(authApp).listen(3000);
+const app = new Elysia()
+	.get("/", () => "Hello World")
+	.use(
+		logger({
+			autoLogging: true,
+			errorKey: "error",
+		}),
+	)
+	.use(handlerErrors)
+	.use(authApp)
+	.listen(3000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
