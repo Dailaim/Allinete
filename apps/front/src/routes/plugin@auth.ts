371a35202dc371332a53b5bf1d22d1ef0cad1d83
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-10 11:41:20 -0500

    Add authentication functionality with GitHub and credentials provider

--- apps/front/src/routes/plugin@auth.ts
+++ apps/front/src/routes/plugin@auth.ts
@@ -1,10 +1,37 @@
 import type { AuthConfig } from "@auth/core";
+import wretch from "wretch";
 
 import GitHub from "@auth/core/providers/github";
+
+import Credentials from "@auth/core/providers/credentials";
+
 import { serverAuth$ } from "@builder.io/qwik-auth";
 
+import type { User as AuthUser } from "@auth/core/types";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { $ } from "@builder.io/qwik";
 import { prismaAuth } from "@database/auth";
+
+const authFetch = wretch("http://localhost:3000/").options({
+	credentials: "include",
+});
+
+interface User {
+	name: string;
+	email: string;
+	password: string;
+}
+
+const Login = $((type: "signin" | "signup", user: User) => {
+	return authFetch
+		.url(type)
+		.post(user)
+		.unauthorized((e) => {
+			console.log("error", e);
+			return null;
+		})
+		.json((r) => r.user as Promise<User>);
+});
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
 	serverAuth$(
@@ -12,12 +39,49 @@ export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
 			({
 				secret: env.get("AUTH_SECRET"),
 				trustHost: true,
+				debug: false,
+
 				providers: [
+					Credentials({
+						credentials: {
+							name: { label: "Name", type: "text" },
+							email: { label: "Email", type: "text" },
+							password: { label: "Password", type: "password" },
+							isRegister: { label: "isRegister", type: "checkbox" },
+						},
+
+						async authorize(credentials) {
+							const { isRegister, callbackUrl, ...user } =
+								credentials as User & {
+									isRegister: string;
+								};
+							const authUser = (await Login(
+								isRegister === "true" ? "signup" : "signin",
+								user,
+							).catch(() => {
+								return null;
+							})) as AuthUser | null;
+
+							return authUser;
+						},
+					}),
+
 					GitHub({
 						clientId: env.get("GITHUB_ID"),
 						clientSecret: env.get("GITHUB_SECRET"),
 					}),
 				],
+
 				adapter: PrismaAdapter(new prismaAuth()),
+
+				callbacks: {
+					redirect({ url }) {
+						return url;
+					},
+				},
+
+				pages: {
+					error: "/",
+				},
 			}) as AuthConfig,
 	);
