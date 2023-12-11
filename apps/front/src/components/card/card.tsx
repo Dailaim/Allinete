7585ee1cf71f0fcc885ff1b556bd7b395f739bf7
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-10 21:55:32 -0500

    Refactor Card component

--- apps/front/src/components/card/card.tsx
+++ apps/front/src/components/card/card.tsx
@@ -1,5 +1,5 @@
 import type { HtmlHTMLAttributes, PropFunction } from "@builder.io/qwik";
-import { type QRL, component$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 
 import { useNavigate } from "@builder.io/qwik-city";
 import { Button } from "../button";
@@ -29,98 +29,100 @@ export type CardProps = {
 // TODO: Add the other variants
 // Create varian Out of Stock
 
-export const Card = component$<CardProps>((props) => {
-	const nav = useNavigate();
-	return (
-		<div
-			role="link"
-			onClick$={() => {
-				if (props.href) {
-					nav(props.href);
-				}
-			}}
-			aria-labelledby={`card-${props.name}`}
-			href={props.href ?? "#"}
-			class={[
-				"group w-72 font-montserrat  justify-center items-center bg-white flex gap-2.5 flex-col rounded overflow-hidden relative",
-				//@ts-ignore
-				props.class,
-			]}
-		>
-			{props.href && (
-				<a href={props.href} class="sr-only">
-					{props.name}
-				</a>
-			)}
-
-			<span class="w-full h-full">
-				<img
-					alt={props.name}
-					src={props.image}
-					class="aspect-square object-cover object-center w-full overflow-hidden self-stretch"
-				/>
-			</span>
-			<Heart
-				glitter={props.isWishlist}
-				onClick$={props.toggleWishlist$}
-				class={{
-					"absolute right-0 top-0 mt-5 mr-5 group-hover:block hidden": true,
-					"!block": props.isWishlist,
+export const Card = component$<CardProps>(
+	({ toggleWishlist$, Action$, href, ...props }) => {
+		const nav = useNavigate();
+		return (
+			<div
+				role="link"
+				onClick$={() => {
+					if (href) nav(href);
 				}}
-			/>
-			{/* TODO: Create component tag and login in card for decorator.
+				aria-labelledby={`card-${props.name}`}
+				href={href ?? "#"}
+				class={[
+					"group w-72 font-montserrat  justify-center items-center bg-white flex gap-2.5 flex-col rounded overflow-hidden relative",
+					//@ts-ignore
+					props.class,
+				]}
+			>
+				{href && (
+					<a href={href} class="sr-only">
+						{props.name}
+					</a>
+				)}
+
+				<span class="w-full h-full">
+					<img
+						alt={props.name}
+						src={props.image}
+						class="aspect-square object-cover object-center w-full overflow-hidden self-stretch"
+					/>
+				</span>
+				<Heart
+					glitter={props.isWishlist}
+					// eslint-disable-next-line qwik/valid-lexical-scope
+					onClick$={toggleWishlist$}
+					class={{
+						"absolute right-0 top-0 mt-5 mr-5 group-hover:block hidden": true,
+						"!block": props.isWishlist,
+					}}
+				/>
+				{/* TODO: Create component tag and login in card for decorator.
         Example: discount, new, etc
       */}
-			{/* {props.offerPercentage && (
+				{/* {props.offerPercentage && (
 				<Badge class="absolute left-0 top-0 mt-5 bg-pink">
 					-{props.offerPercentage}%
 				</Badge>
 			)} */}
 
-			<div class="flex w-full  flex-col mb-5 items-center px-5">
-				<div class="flex flex-col pb-5 gap-1 w-full">
-					<h4 class="capitalize line-clamp-2 group-hover:text-pink font-poppins ">
-						{props.name}
-					</h4>
-					<div class="text-sm gap-1 items-center flex">
-						<RatingStar
-							rating={props.userRating ?? 0}
-							changeRating$={props.setUserRating$}
-							class="gap-1.5"
-							StarProps={{
-								class: "w-3.5 h-3.5",
-							}}
-						/>{" "}
-						({props.rating ?? 0})
-					</div>
-					<span class="text-blue-gray text-xs line-clamp-2 leading-[157.143%]">
-						{props.description}
-					</span>
-
-					<div class="text-xs text-blue-gray">
-						<span class={{ "line-through": props.isOffer }}>
-							{props.currency} {props.price}
+				<div class="flex w-full  flex-col mb-5 items-center px-5">
+					<div class="flex flex-col pb-5 gap-1 w-full">
+						<h4 class="capitalize line-clamp-2 group-hover:text-pink font-poppins ">
+							{props.name}
+						</h4>
+						<div class="text-sm gap-1 items-center flex">
+							<RatingStar
+								rating={props.userRating ?? 0}
+								changeRating$={props.setUserRating$}
+								class="gap-1.5"
+								StarProps={{
+									class: "w-3.5 h-3.5",
+								}}
+							/>{" "}
+							({props.rating ?? 0})
+						</div>
+						<span class="text-blue-gray text-xs line-clamp-2 leading-[157.143%]">
+							{props.description}
 						</span>
-						{props.isOffer && (
-							<span class="text-pink pl-1">
-								{props.currency} {props.offerPrice}
-							</span>
-						)}
-					</div>
-				</div>
 
-				<Button
-					onClick$={(e) => {
-						e.stopPropagation();
-						props.Action$?.();
-					}}
-					type="button"
-					class="w-full"
-					variant="secondary"
-				>
-					Agregar al Carrito{" "}
-				</Button>
+						<div class="text-xs text-blue-gray">
+							<span class={{ "line-through": props.isOffer }}>
+								{props.currency} {props.price}
+							</span>
+							{props.isOffer && (
+								<span class="text-pink pl-1">
+									{props.currency} {props.offerPrice}
+								</span>
+							)}
+						</div>
+					</div>
+
+					<Button
+						onClick$={(e) => {
+							e.stopPropagation();
+							// eslint-disable-next-line qwik/valid-lexical-scope
+							Action$?.();
+						}}
+						type="button"
+						class="w-full"
+						variant="secondary"
+					>
+						Agregar al Carrito{" "}
+					</Button>
+				</div>
 			</div>
-		</div>
-	);
-});
+		);
+	},
+);
