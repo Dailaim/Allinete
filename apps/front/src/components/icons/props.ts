9b484be5a5013f32255a21d4085d905e3bdc6f17
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-10 21:55:51 -0500

    Update SVGProps type definition

--- apps/front/src/components/icons/props.ts
+++ apps/front/src/components/icons/props.ts
@@ -1,7 +1,11 @@
-import type { HtmlHTMLAttributes } from "@builder.io/qwik";
+import type {
+	HtmlHTMLAttributes,
+	QwikIntrinsicElements,
+} from "@builder.io/qwik";
 import type { LayoutSVGWithNamePros } from "./layoutSvgWithName";
 
-export type SVGProps = HtmlHTMLAttributes<SVGElement>;
+export type SVGProps = HtmlHTMLAttributes<SVGElement> &
+	QwikIntrinsicElements["svg"];
 export type SVGWithNameProps = LayoutSVGWithNamePros & {
 	name?: string;
 	svgProps?: SVGProps;
