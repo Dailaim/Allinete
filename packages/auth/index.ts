d36262465f856b4ea8daa55003a0e1f8e41612cd
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-10 21:56:31 -0500

    Fix import formatting in auth package

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,3 +1,3 @@
-export { PrismaClient as prismaAuth } from '@prisma/client'
+export { PrismaClient as prismaAuth } from "@prisma/client";
 
-export { PrismaClient as prismaAuthEdge } from '@prisma/client/edge'
+export { PrismaClient as prismaAuthEdge } from "@prisma/client/edge";
