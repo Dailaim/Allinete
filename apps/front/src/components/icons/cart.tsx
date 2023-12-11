4c88eef2fcd6fc9193451c7a4b074be59b7f4dda
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-10 21:55:37 -0500

    Remove empty role attribute from SVGCart component

--- apps/front/src/components/icons/cart.tsx
+++ apps/front/src/components/icons/cart.tsx
@@ -6,7 +6,6 @@ export const SVGCart = component$<SVGProps>(
 	({ class: ClassName, ...props }) => {
 		return (
 			<svg
-				role=""
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
 				fill="none"
